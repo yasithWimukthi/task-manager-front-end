0b6fee0e7968522abd90f9418f88039c635a1652
yasithw <yasith.w@eight25media.com>
2023-12-16 12:57:11 +0530

    create add task form

--- src/pages/HomePage/Index.jsx
+++ src/pages/HomePage/Index.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import './HomePage.css';
 import {Button, Container} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
+import TaskForm from "../../components/TaskForm/TaskForm";
 
 const HomePage = () => {
+    const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false);
+
+    const handleAddTaskFormOpen = () => {
+        setIsAddTaskFormOpen(true);
+    };
+
+    const handleAddTaskFormClose = () => {
+        setIsAddTaskFormOpen(false);
+    };
+
     return (
         <div>
             <h1>To do list</h1>
+            <TaskForm
+                title="Add Task"
+                open={isAddTaskFormOpen}
+                onClose={handleAddTaskFormClose}
+                onSubmit={handleAddTaskFormClose}
+            />
             <Container maxWidth="xl" className="task-container">
-                <Button className="add-btn" color="success" variant="contained" endIcon={<AddIcon />} style={{borderRadius:'50%',width:'100px',height:'100px'}}></Button>
+                <Button
+                    className="add-btn"
+                    color="success"
+                    variant="contained"
+                    endIcon={<AddIcon />}
+                    style={{borderRadius:'50%',width:'100px',height:'100px'}}
+                    onClick={handleAddTaskFormOpen}>
+                </Button>
             </Container>
         </div>
     );
