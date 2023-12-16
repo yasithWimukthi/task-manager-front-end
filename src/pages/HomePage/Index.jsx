17ebcff423df0b73309e09d1dee579efe145754c
yasithw <yasith.w@eight25media.com>
2023-12-16 12:01:36 +0530

    create home page

--- src/pages/HomePage/Index.jsx
+++ src/pages/HomePage/Index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import './HomePage.css';
-import {Box, Container} from "@mui/material";
+import {Button, Container} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 const HomePage = () => {
     return (
         <div>
             <h1>To do list</h1>
-            <Container maxWidth="xl">
-                <Box sx={{bgcolor: '#cfe8fc', height: '100vh'}}/>
+            <Container maxWidth="xl" className="task-container">
+                <Button className="add-btn" color="success" variant="contained" endIcon={<AddIcon />} style={{borderRadius:'50%',width:'100px',height:'100px'}}></Button>
             </Container>
         </div>
     );
